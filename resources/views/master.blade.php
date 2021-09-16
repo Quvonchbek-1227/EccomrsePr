376dbd5a68d27555a0353abc872262de41eb7e0d
Quvonchbek-1227 <75905122+Quvonchbek-1227@users.noreply.github.com>
2021-09-16 10:31:53 +0300

    'test'

---
+++ resources/views/master.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css')}}">
+    <title>Document</title>
+</head>
+<body>
+    {{ View::make('header') }}
+    @yield('content')
+    {{ View::make('footer') }}
+    
+
+
+
+    <script src="{{ asset('jquery.min.js') }}"></script>
+    <script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
+</body>
+<style>
+    .custom-login{
+        height: 500px;
+        padding-top: 100px
+    }
+</style>
+</html>
